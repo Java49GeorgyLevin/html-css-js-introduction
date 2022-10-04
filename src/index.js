a69bbe978def2e19c92ff1fe57643f3b5ec0d49c
Georgy Levin <g.phyzy@gmail.com>
2022-10-04 17:49:05 +0300

    HW#8 js click

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const anchors = document.querySelectorAll(".thumbnails-anchor");
+const detailsImage = document.querySelector(".details-image");
+const detailsTitle = document.querySelector(".details-title");
+const detailsYear = document.querySelector(".details-year");
+function setDetails(anchor) {
+    detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
+    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    detailsYear.innerHTML = anchor.getAttribute("data-details-year");
+}
+for(let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener("click", function() {
+        setDetails(anchors[i]);
+    })
+}
+
