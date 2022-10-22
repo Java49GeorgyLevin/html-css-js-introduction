ec2dd2dde1807941e96dab598f5b88627d859faf
Georgy Levin <g.phyzy@gmail.com>
2022-10-22 08:52:48 +0300

    Portrait hide & show

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const anchors = document.querySelectorAll(".thumbnails-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const detailsYear = document.querySelector(".details-year");
+const mainClass = document.querySelector(".main-class");
+const HIDDEN = "hidden";
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
@@ -9,6 +11,14 @@ function setDetails(anchor) {
 }
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
-        setDetails(anchors[i]);
+        showDetails();
+        setDetails(anchors[i]);                
     })
+}
+function showDetails() {
+    mainClass.classList.remove(HIDDEN);
+}
+
+function hideDetails() {
+    mainClass.classList.add(HIDDEN);
 }
