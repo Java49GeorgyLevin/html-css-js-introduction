cc582bed4e9eb6ea306f31814ce65b90f1f5b575
Georgy Levin <g.phyzy@gmail.com>
2022-10-24 23:07:22 +0300

    Animation with audio

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
 const detailsYear = document.querySelector(".details-year");
 const mainClass = document.querySelector(".main-class");
+const detailsContainer = document.querySelector(".details-container");
+const detailsSound = document.querySelector(".details-sound");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 function setDetails(anchor) {
     detailsImage.setAttribute("src", anchor.getAttribute("data-details-image"));
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
     detailsYear.innerHTML = anchor.getAttribute("data-details-year");
+    detailsSound.setAttribute("src", anchor.getAttribute("data-details-sound"));
 }
 for(let i = 0; i < anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
@@ -15,8 +19,16 @@ for(let i = 0; i < anchors.length; i++) {
         setDetails(anchors[i]);                
     })
 }
+
 function showDetails() {
     mainClass.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+        },1);  
+    setTimeout(function(){
+        detailsSound.setAttribute("src", null);
+    },3500);
 }
 
 function hideDetails() {
