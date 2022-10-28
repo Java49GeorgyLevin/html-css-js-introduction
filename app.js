599e24645ce938cb8d82103d55be29002a3fd02f
Georgy Levin <g.phyzy@gmail.com>
2022-10-28 09:41:07 +0300

    HW11 sumDigits

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-
-// console.log("Hello world");
-// for( i = 0; i < 3; i++) {
-//     setTimeout(function(){
-//         console.log(i)
-//     });
-// }
-i = 100;
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(function () { console.log(i) });
-// } => 0 1 2
-
-// for (var i = 0; i < 3; i++) {
-//      console.log(i) ;
-// }
-// function sum(op1, op2) {
-//     let res = op1 + op2;
-//     return res;
-// }
-// let op1 = 10;
-// let op2 = 20;
-// let res = sum(op1, op2);
-// console.log(res)
 function sumDigits(number) {
-    //TODO
-    //function should return sum of a given number digits
+    if(number < 0)
+    number = -number; 
+    let sum = 0;
+    do {
+        number = number.toFixed(0);
+        sum += number % 10;
+        number /= 10;     
+    }
+    while(number >= 1);
+    return sum; 
 }
-//Example
-console.log(sumDigits(123)); //should be printed out 6
+console.log(sumDigits(-1231.11));
