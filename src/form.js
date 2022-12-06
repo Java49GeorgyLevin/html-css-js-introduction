21403cb49168965edb6d1f221e4d65a3ba5f6daa
Georgy Levin <g.phyzy@gmail.com>
2022-12-07 01:05:21 +0200

    HW22 2/3 (button3 not ready)

--- src/form.js
+++ src/form.js
@@ -10,6 +10,15 @@ const company = new Company();
 const dateErrorElement = document.getElementById("date_error");
 const salaryErrorElement = document.getElementById("salary_error");
 
+const menuSection = document.querySelectorAll("section");
+const menuButton = document.querySelectorAll(".menu-button");
+const ACTIVE_BUTTON = "active-button";
+
+const allEmployees = document.querySelector(".all-employees");
+const salaryEmployees = document.querySelector(".salary-employees");
+const salaryFrom = -1;
+const salaryTo = -1;
+
 function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
@@ -21,6 +30,7 @@ function onSubmit(event) {
     )
     console.log(employee)
     company.hireEmployee(employee);
+    employeeAdded(employee.employee_name);
     
 }
 function onChange(event) {
@@ -73,5 +83,75 @@ Company.prototype.getAllEmployees = function(){
     return this.employees;
 }
 Company.prototype.getEmployeesBySalary = function(salaryFrom, salaryTo) {
-    //TODO
+    return company.getAllEmployees().filter(empl => {
+        if(empl.salary > salaryFrom && empl.salary < salaryTo)
+        return empl; 
+    })
+}
+
+function showMenu(index) {
+    menuSection.forEach (section => section.hidden = true);
+    menuButton.forEach (button => button.classList.remove(ACTIVE_BUTTON));
+    menuSection[index].hidden = false;
+    menuButton[index].classList.add(ACTIVE_BUTTON);
+    if(index == 1) {
+        showAllEmployees();
+    }
+
+}
+
+function employeeAdded(name) {
+    // console.log("add", name);
+    const confirmationAdded = document.getElementById("the-employee-added");
+    confirmationAdded.innerHTML = `${name} added successfully`;
+    setTimeout(() => confirmationAdded.innerHTML = '', 3000);
+}
+
+function listEmployees() {
+    console.log(company.getAllEmployees());
+
+    const arEmpl = company.getAllEmployees().map(empl => 
+    // getAllEmployees.forEach(empl => {
+    // .map(empl => {
+       
+       employeeData(empl));
+      // allEmployees.innerHTML = `${empl}`
+    //   console.log(employeeData(empl));
+      return arEmpl.join('');
+    } 
+    // return "Hi!"
+// }
+function listSalary() {
+    const arEmpl = company.getEmployeesBySalary().map(empl =>
+        employeeData(empl));
+        return arEmpl.join('');
+
+        // )
+}
+
+function employeeData(empl) {
+    return `<li class="cart">
+    <p>Name: ${empl.employee_name}</p>
+    <p>Email: ${empl.email}</p>
+    <p>BirthDate: ${empl.birthDate}</p>
+    <p>Department: ${empl.department}</p>
+    <p>Salary: ${empl.salary}</p>
+    </li>`
+}
+
+function showAllEmployees() {
+allEmployees.innerHTML = listEmployees();
+}
+
+function showSalaryEmployees() {
+    salaryEmployees.innerHTML = listSalary();
+
+}
+function salaryMinMax(event) {
+    salaryFrom = event.target[salary-from].value;
+    salaryTo = event.target.salary-to.value;
+    // if (event.target.name == "salaryFrom"
+    // name="salary-from"
+    // name="salary-to"
+    showSalaryEmployees();
 }
